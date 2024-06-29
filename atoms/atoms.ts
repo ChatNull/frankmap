77586f7832a77cc92666f2ab2017c4cccfa4ec0e
woojoung <ywj981217@naver.com>
2024-06-29 20:09:06 +0900

    🐛 [FE] fix: 배포 버그수정 atom record type 삭제

--- atoms/atoms.ts
+++ atoms/atoms.ts
@@ -13,12 +13,12 @@ interface ModalType {
 
 export const dataState = atom({
   key: "dataState",
-  default: [] as RecordType[],
+  default: [],
 });
 
 export const FilteredData = atom({
   key: "filteredData",
-  default: [] as RecordType[],
+  default: [],
 });
 
 export const emotionState = atom({
