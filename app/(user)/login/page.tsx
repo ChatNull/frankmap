627b973d658fc8f75de6fc6c352a3e131ead359f
전희선 <designh2sun@gmail.com>
2024-07-28 21:25:53 +0900

    🚧 [FE] cont: 유저 정보 전역값으로 저장

--- app/(user)/login/page.tsx
+++ app/(user)/login/page.tsx
@@ -8,17 +8,55 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import "./login.scss";
 import Button from "@/components/button/button";
+import { Metadata } from "next";
+import { useModal } from "@/hooks/useModal";
+import Input from "@/components/input/input";
+
+export function loginMetadata({ params }: { params: { id: number } }): Metadata {
+  return {
+    title: "로그인",
+    description: "로그인 정보를 입력 해 주세요.",
+    openGraph: {
+      title: "솔직할지도",
+      description: "로그인 정보를 입력 해 주세요.",
+      images: "/opengraph_img.png",
+      url: `https://frankmap.netlify.app/login`,
+      type: "website",
+      siteName: "솔직할지도",
+    },
+  };
+}
 
 interface LoginFormInputs {
   email: string;
   password: string;
+  name: string;
 }
 
 export default function Login() {
-  const setUser = useSetRecoilState(userState);
-  const getUser = useRecoilValue(userState);
+  const setUser = useSetRecoilState(userState); // Recoil 상태 업데이트
+  const getUser = useRecoilValue(userState); // 현재 유저 상태
   const router = useRouter();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { openModal } = useModal();
+
+  const handleModal = () => {
+    const modalData = {
+      content: !isLoggedIn
+        ? `
+      <img className="emotion" src="/emotion2.svg" alt="환영합니다" />
+      <h1>로그인 성공!</h1>
+      <p>오늘의 감정을 남겨주세요</p>
+      `
+        : `<img className="emotion" src="/emotion4.svg" alt="로그아웃" />
+        <h1>로그아웃 완료</h1>
+          <p>다음에 다시 만나요</p>
+        `,
+      button: "확인",
+      callBack: () => router.push(`/`),
+    };
+    openModal(modalData);
+  };
 
   const {
     register,
@@ -27,14 +65,11 @@ export default function Login() {
   } = useForm<LoginFormInputs>();
 
   useEffect(() => {
-    console.log("유저정보 변경됐누:", getUser);
-  }, [getUser]);
-
-  useEffect(() => {
     const checkLoginStatus = async () => {
       const { data, error } = await supabase.auth.getUser();
       if (data?.user) {
-        setUser(data.user.id);
+        // 사용자 정보를 Recoil 상태로 설정
+        // setUser({ id: data.user.id, email: data.user.email, name: 'ddd' });
         setIsLoggedIn(true);
       }
     };
@@ -43,6 +78,7 @@ export default function Login() {
 
   const handleLogin: SubmitHandler<LoginFormInputs> = async (data) => {
     const { email, password } = data;
+
     try {
       const { data: loginData, error } = await supabase.auth.signInWithPassword({
         email,
@@ -50,16 +86,19 @@ export default function Login() {
       });
 
       if (error) throw error;
-
       if (loginData.user) {
-        setUser(loginData.user.id);
+        // 로그인 성공 시 Recoil 상태로 유저 정보 설정
+        setUser({
+          id: loginData.user.id,
+          email: loginData.user.email,
+          nickname: loginData.user.user_metadata.nickname,
+        });
         setIsLoggedIn(true);
       }
 
-      alert("로그인 성공!");
-      router.replace("/"); // 로그인 성공 후 메인 페이지로 이동
+      handleModal();
+      // router.replace("/");
     } catch (error: any) {
-      console.error("로그인 에러:", error);
       alert(error.message);
     }
   };
@@ -67,25 +106,41 @@ export default function Login() {
   const handleLogout = async () => {
     try {
       const { error } = await supabase.auth.signOut();
+
       if (error) throw error;
-      setUser(null);
+      setUser(null); // 로그아웃 시 Recoil 상태 초기화
       setIsLoggedIn(false);
       localStorage.removeItem("supabase.auth.token"); // 로컬 스토리지에서 토큰 제거
       localStorage.removeItem("userState"); // userState 삭제
-      alert("로그아웃 성공!");
+      router.replace("/");
+      handleModal();
+
       router.replace("/login"); // 로그아웃 후 로그인 페이지로 이동
     } catch (error: any) {
-      console.error("로그아웃 에러:", error);
       alert(error.message);
     }
+  };
+
+  const handleSignup = () => {
+    router.push("/signup");
   };
 
   if (isLoggedIn) {
     return (
       <div className="login-container">
         <p>이미 로그인된 상태입니다.</p>
-        <button onClick={() => router.replace("/")}>메인 페이지로 이동</button>
-        <button onClick={handleLogout}>로그아웃</button>
+        <div className="login-group">
+          <Button handleClick={() => router.replace("/")}>메인 페이지로 이동</Button>
+          <Button
+            size="normal"
+            color="secondary"
+            handleClick={() => {
+              handleLogout();
+            }}
+          >
+            로그아웃
+          </Button>
+        </div>
       </div>
     );
   }
@@ -95,8 +150,7 @@ export default function Login() {
       <h1>로그인</h1>
       <form onSubmit={handleSubmit(handleLogin)} className="login-form">
         <div className="form-group">
-          <input
-            type="email"
+          <Input
             {...register("email", {
               required: "이메일을 입력해주세요.",
               pattern: {
@@ -104,23 +158,31 @@ export default function Login() {
                 message: "유효한 이메일 주소를 입력해주세요.",
               },
             })}
+            id="email"
             placeholder="이메일"
           />
           {errors.email && <p className="error-message">{errors.email.message}</p>}
         </div>
         <div className="form-group">
-          <input
-            type="password"
+          <Input
             {...register("password", {
               required: "비밀번호를 입력해주세요.",
             })}
+            id="password"
+            type="password"
             placeholder="비밀번호"
           />
           {errors.password && <p className="error-message">{errors.password.message}</p>}
         </div>
-        <button type="submit" className="submit-button">
-          로그인
-        </button>
+
+        <div className="button-group">
+          <Button type="submit" size="full" color="secondary">
+            로그인
+          </Button>
+          <Button type="button" size="full" color="primary" handleClick={handleSignup}>
+            회원가입
+          </Button>
+        </div>
       </form>
     </div>
   );
--- app/(user)/signup/page.tsx
+++ app/(user)/signup/page.tsx
@@ -1,9 +1,32 @@
+// SignUp.tsx
+
 "use client";
 
 import { useForm, SubmitHandler } from "react-hook-form";
 import { supabase } from "@/libs/supabase";
 import "./signup.scss";
 import Button from "@/components/button/button";
+import { useRouter } from "next/navigation";
+import Input from "@/components/input/input";
+import { userState } from "@/atoms/userstate";
+import { useSetRecoilState } from "recoil";
+import { Metadata } from "next";
+import { useModal } from "@/hooks/useModal";
+
+export function signupMetadata({ params }: { params: { id: number } }): Metadata {
+  return {
+    title: "회원가입",
+    description: "회원 정보를 입력 해 주세요.",
+    openGraph: {
+      title: "솔직할지도",
+      description: "회원 정보를 입력 해 주세요.",
+      images: "/opengraph_img.png",
+      url: `https://frankmap.netlify.app/signup`,
+      type: "website",
+      siteName: "솔직할지도",
+    },
+  };
+}
 
 interface SignUpFormInputs {
   email: string;
@@ -12,29 +35,53 @@ interface SignUpFormInputs {
 }
 
 export default function SignUp() {
+  const { openModal } = useModal();
+  const router = useRouter();
+  const setUser = useSetRecoilState(userState);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<SignUpFormInputs>();
 
+  const handleModal = () => {
+    const modalData = {
+      content: `
+      <img className="emotion" src="/emotion2.svg" alt="환영합니다" />
+      <h1>회원가입 성공</h1>
+      <p>반가워요 :) <br>오늘 하루는 어땠나요?</p>
+      `,
+      button: "홈으로",
+      callBack: () => router.push(`/`),
+    };
+    openModal(modalData);
+  };
+
   const handleSignUp: SubmitHandler<SignUpFormInputs> = async (data) => {
     const { email, password, nickname } = data;
-    console.log("x", data);
 
     try {
       const { data: signUpData, error } = await supabase.auth.signUp({
-        email: email,
-        password: password,
+        email,
+        password,
         options: {
           data: {
-            nickname: nickname,
+            nickname, // user_metadata에 저장
           },
         },
       });
+
       if (error) throw error;
-      console.log(signUpData);
-      alert("회원가입이 완료되었습니다!");
+      setUser(signUpData);
+      if (signUpData.user) {
+        setUser({
+          id: signUpData.user.id,
+          email: signUpData.user.email,
+          nickname: signUpData.user.user_metadata.nickname,
+        });
+      }
+
+      handleModal();
     } catch (error: any) {
       alert(error.message);
     }
@@ -45,12 +92,19 @@ export default function SignUp() {
       <h1>회원가입</h1>
       <form onSubmit={handleSubmit(handleSignUp)} className="signup-form">
         <div className="form-group">
-          <input type="text" {...register("nickname", { required: "닉네임을 입력해주세요." })} placeholder="닉네임" />
+          <Input
+            {...register("nickname", {
+              required: "닉네임을 입력해주세요.",
+            })}
+            id="name"
+            placeholder="닉네임"
+            type="text"
+          />
           {errors.nickname && <p className="error-message">{errors.nickname.message}</p>}
         </div>
+
         <div className="form-group">
-          <input
-            type="email"
+          <Input
             {...register("email", {
               required: "이메일을 입력해주세요.",
               pattern: {
@@ -58,23 +112,28 @@ export default function SignUp() {
                 message: "유효한 이메일 주소를 입력해주세요.",
               },
             })}
+            id="email"
+            // type="email"
             placeholder="이메일"
           />
           {errors.email && <p className="error-message">{errors.email.message}</p>}
         </div>
+
         <div className="form-group">
-          <input
-            type="password"
+          <Input
             {...register("password", {
               required: "비밀번호를 입력해주세요.",
             })}
+            id="password"
+            type="password"
             placeholder="비밀번호"
           />
           {errors.password && <p className="error-message">{errors.password.message}</p>}
         </div>
-        <button type="submit" className="submit-button">
+
+        <Button type="submit" size="full" color="secondary">
           가입하기
-        </button>
+        </Button>
       </form>
     </div>
   );
